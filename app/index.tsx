01610eb67c3e046b0adbe1020b683d6b75670e6b
Alema Edrick <150422248+edrickalema@users.noreply.github.com>
2025-05-17 12:30:40 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -121,6 +121,35 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
 });
-
+// import * as React from "react";
+// import * as WebBrowser from "expo-web-browser";
+// import * as Google from "expo-auth-session/providers/google";
+// import { Button } from "react-native";
+// import { auth } from "../../firebaseConfig";
+// import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+ 
+// WebBrowser.maybeCompleteAuthSession();
+ 
+// export default function GoogleSignInButton() {
+//   const [request, response, promtAsync] = Google.useIdTokenAuthRequest({
+//     clientId: "yourCliendId.apps.googleusercontent.com",
+//   });
+ 
+//   React.useEffect(() => {
+//     if (response?.type === "success") {
+//       const { id_token } = response.params;
+//       const credential = GoogleAuthProvider.credential(id_token);
+//       signInWithCredential(auth, credential);
+//     }
+//   }, [response]);
+ 
+//   return (
+//     <Button
+//       title="Sign In with Google"
+//       disabled={!request}
+//       onPress={() => promtAsync()}
+//     />
+//   );
+// }
 // Stack ->	A stack navigator is a type of navigation container that manages a stack of screens. Each screen is pushed onto the stack, and when a screen is popped, it is removed from the stack.
 // Tabs ->	A tab navigator is a type of navigation container that manages a set of tabs. Each tab is a screen, and when a tab is selected, it is displayed.
